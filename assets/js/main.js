9fa80cbee5cf1a8d1fcff26668aa5531e86fef6f
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-03-12 16:45:38 -0300

    Consumindo a PokeApi e manipulando o HTML com javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+function convertpokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+                        alt=${pokemon.name}>
+                </div>
+            </li>`
+}
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+const pokemonLi = document.getElementById("pokemonList")
+
+pokeApi.getPokemons()
+    .then((pokemons = []) => {
+        /* Pode ser assim
+        const newList = pokemons.map((pokemon) => {
+            return convertpokemonToLi(pokemon)
+        })
+        assim
+        const newList = pokemons.map((pokemon) => convertpokemonToLi(pokemon))
+        ou direto como esta abaixo*/
+        pokemonLi.innerHTML += pokemons.map(convertpokemonToLi).join('')
+
+    })
