359a0f69777d1f46a85d066f230113610b6335f1
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-03-13 16:17:55 -0300

    Pokedex finalizada com paginação de pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,49 @@
+const pokemonLi = document.getElementById("pokemonList")
+const loadMoreButton = document.getElementById("loadMoreButton")
+const limit = 6
+let offset = 0
+const maxRecords = 15
+
+
 function convertpokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-                <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(' ')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+                    <img src=${pokemon.photo}
                         alt=${pokemon.name}>
                 </div>
             </li>`
 }
 
-const pokemonLi = document.getElementById("pokemonList")
-
-pokeApi.getPokemons()
-    .then((pokemons = []) => {
-        /* Pode ser assim
-        const newList = pokemons.map((pokemon) => {
-            return convertpokemonToLi(pokemon)
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit)
+        .then((pokemons = []) => {
+            /* Pode ser assim
+            const newList = pokemons.map((pokemon) => {
+                return convertpokemonToLi(pokemon)
+            })
+            assim
+            const newList = pokemons.map((pokemon) => convertpokemonToLi(pokemon))
+            ou direto como esta abaixo*/
+            pokemonLi.innerHTML += pokemons.map(convertpokemonToLi).join('')
         })
-        assim
-        const newList = pokemons.map((pokemon) => convertpokemonToLi(pokemon))
-        ou direto como esta abaixo*/
-        pokemonLi.innerHTML += pokemons.map(convertpokemonToLi).join('')
+}
+loadPokemonItens(offset, limit)
 
-    })
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,34 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertPokeApiDetailTopokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+
+    pokemon.types = types
+    pokemon.type = types[0]
+
+    pokemon.photo = pokeDetail.sprites.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailTopokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 6) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests => Promise.all(detailRequests)))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
 }
